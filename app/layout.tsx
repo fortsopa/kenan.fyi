78924b60ca27c193ebe97d1064895c2a8235da70
fortsopa <kenan.akinci@icloud.com>
2023-02-25 23:11:11 +0100

    Tailwind animation, font options

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 import "../styles/globals.css";
+import { Noto_Serif } from "@next/font/google";
+import Typical from "react-typical";
+
+const notoSerif = Noto_Serif({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function RootLayout({
   children,
@@ -8,11 +15,11 @@ export default function RootLayout({
 }) {
   const header = (
     <header>
-      <div className='text-center  p-1 my-1 rounded-md'>
+      <div className='text-center p-1 my-1  rounded-md'>
         <Link href='/'>
-          <h1 className='text-3xl font-bold text-slate-600'>kenan.fyi</h1>
+          <h1 className='text-5xl font-bold text-slate-600'>kenan</h1>
+          <h3 className='text-3xl text-right font-bold text-slate-600'>.fyi</h3>
         </Link>
-        <p className='text-gray-800'>droppings from my second brain</p>
       </div>
     </header>
   );
@@ -20,6 +27,9 @@ export default function RootLayout({
   const footer = (
     <footer>
       <div className=' mt-1 py-1  text-center text-slate-700'>
+        <p className='animate-typing overflow-hidden whitespace-nowrap text-gray-700 italic pb-10 text-2xl'>
+          droppings from my second brain...
+        </p>
         <h3> Under Construction for a while.</h3>
         <p>
           Reach me out
@@ -33,7 +43,7 @@ export default function RootLayout({
   );
 
   return (
-    <html>
+    <html className={notoSerif.className}>
       <head />
       <body className='bg-eigen'>
         <div className='h-screen flex flex-col justify-center items-center mx-auto'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,28 @@ module.exports = {
       colors: {
         eigen: "#131217",
       },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent",
+          },
+          "100%": {
+            borderColor: "white",
+          },
+        },
+      },
+      animation: {
+        typing: "typing 3s steps(50)  alternate, blink .7s infinite",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
